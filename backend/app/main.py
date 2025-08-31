359c7f827dee9c33a6fccba75cac1663c50c81d9
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-08-31 12:54:34 -0500

    delete

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.openapi_config import custom_openapi
 from app.routes import router as api_router
 
 app = FastAPI()
 
 app.openapi = lambda: custom_openapi(app)
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # URL de tu frontend (Vite por defecto)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE, OPTIONS)
+    allow_headers=["*"],  # Permitir todos los headers
+)
 
 app.include_router(api_router)
 
--- merryChristmas/src/routes/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuthStore } from "@/store/auth";
-import type { JSX } from "react";
-
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const { user } = useAuthStore();
-  if (!user) return <Navigate to="/login" replace />;
-  return children;
-}
