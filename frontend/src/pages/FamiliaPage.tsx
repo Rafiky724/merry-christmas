9474bd86e9144462e127dcd6bf8309a35d6b0efe
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 17:06:37 -0500

    Update FamiliaPage.tsx

--- frontend/src/pages/FamiliaPage.tsx
+++ frontend/src/pages/FamiliaPage.tsx
@@ -1,3 +1,21 @@
+import DeseoCard from "../components/DeseoCard";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
 export default function FamiliaPage() {
-  return <div>FamiliaPage</div>;
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Nav />
+
+        <main className="flex-grow p-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            <DeseoCard />
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    </>
+  );
 }
