e41438e757d7526779e288e4db2cfa16962daaee
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 17:14:57 -0500

    Update FamiliaPage.tsx

--- frontend/src/pages/FamiliaPage.tsx
+++ frontend/src/pages/FamiliaPage.tsx
@@ -8,7 +8,7 @@ export default function FamiliaPage() {
       <div className="flex flex-col min-h-screen">
         <Nav />
 
-        <main className="flex-grow p-5">
+        <main className="flex-grow p-5 bg-[url('/patron_navidad.png')] bg-[length:250px_250px] bg-repeat">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             <DeseoCard />
           </div>
