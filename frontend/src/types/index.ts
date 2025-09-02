c221c3913d635200a80942297c77a2709d12ef52
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-02 12:53:43 -0500

    Update index.ts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,3 +1,10 @@
+export type Usuario = {
+    id_usuario: number;
+    nombre: string;
+    correo: string;
+    id_familia?: number; // opcional si tu backend lo permite nulo
+}
+
 export type UsuarioCreate = {
     nombre: string;
     correo: string;
@@ -23,19 +30,25 @@ export type EstadoDeseo = "pendiente" | "comprado";
 export type Deseo = {
     id_deseo: number;
     nombre: string;
-    precio?: number; // Puede ser null/undefined si no se define
-    link?: string;
-    descripcion?: string;
+    precio: number; // Puede ser null/undefined si no se define
+    link: string;
+    descripcion: string;
     id_usuario: number;
     id_familia: number;
     estado: EstadoDeseo;
     fecha_creacion: string; // viene como ISO string desde el backend
-    usuario?: {
-        id_usuario: number;
-        nombre: string;
-    };
-    familia?: {
-        id_familia: number;
-        nombre: string;
-    };
+
+    usuario: Usuario;
+}
+
+export type DeseoCreate = {
+    id_deseo: number;
+    nombre: string;
+    precio: number; // Puede ser null/undefined si no se define
+    link: string;
+    descripcion: string;
+    id_usuario: number;
+    id_familia: number;
+    estado: EstadoDeseo;
+    fecha_creacion: string; // viene como ISO string desde el backend
 }
