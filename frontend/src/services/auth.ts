3627649fd32c8d181eafca85ebaa5519d895e992
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 19:47:53 -0500

    Create auth.ts

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,27 @@
+import axiosInstance from "../api/axiosInstance";
+import qs from "qs";
+
+export const registerUsuario = async (data: {
+    nombre: string;
+    correo: string;
+    contrasena: string;
+    id_familia?: number;
+}) => {
+    const res = await axiosInstance.post("/api/auth/register", data);
+    return res.data;
+};
+
+export const loginUsuario = async (correo: string, contrasena: string) => {
+    const data = qs.stringify({
+        username: correo,
+        password: contrasena,
+    });
+
+    const res = await axiosInstance.post("/api/auth/login", data);
+    return res.data; // { access_token: ..., token_type: ... }
+};
+
+export const logoutUsuario = () => {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("user");
+};
