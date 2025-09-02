f8331f121545a10ebd473ebcdac2c66f51f144b6
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 19:47:57 -0500

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,14 +3,29 @@ import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import FamiliaPage from "../pages/FamiliaPage";
 import RegisterPage from "../pages/RegisterPage";
+import PrivateRoute from "../guards/PrivateRoute";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/familia" element={<FamiliaPage />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/familia"
+        element={
+          <PrivateRoute>
+            <FamiliaPage />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
