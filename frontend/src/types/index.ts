76ee45bccdc20125a540c2661d77bf40c48b0af2
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 19:47:46 -0500

    Update index.ts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,11 +1,41 @@
-export interface UsuarioCreate {
-  nombre: string;
-  correo: string;
-  contrasena: string;
-  id_familia?: number; // opcional si tu backend lo permite nulo
+export type UsuarioCreate = {
+    nombre: string;
+    correo: string;
+    contrasena: string;
+    id_familia?: number; // opcional si tu backend lo permite nulo
 }
 
-export interface LoginData {
-  correo: string;
-  contrasena: string;
+export type LoginData = {
+    correo: string;
+    contrasena: string;
+}
+
+export type Familia = {
+    id_familia: number;
+    codigo: string;
+    nombre: string;
+    fecha_creacion: string; // viene como ISO string del backend
+    creado_por: number;
+}
+
+export type EstadoDeseo = "pendiente" | "comprado";
+
+export type Deseo = {
+    id_deseo: number;
+    nombre: string;
+    precio?: number; // Puede ser null/undefined si no se define
+    link?: string;
+    descripcion?: string;
+    id_usuario: number;
+    id_familia: number;
+    estado: EstadoDeseo;
+    fecha_creacion: string; // viene como ISO string desde el backend
+    usuario?: {
+        id_usuario: number;
+        nombre: string;
+    };
+    familia?: {
+        id_familia: number;
+        nombre: string;
+    };
 }
