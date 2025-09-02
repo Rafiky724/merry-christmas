9c6067ba70be64465e9f45a1486a398dd9a2adc1
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-02 12:53:08 -0500

    Update deseo.py

--- backend/app/schemas/deseo.py
+++ backend/app/schemas/deseo.py
@@ -1,6 +1,9 @@
+from datetime import date
 from pydantic import BaseModel
 from typing import Optional
 from enum import Enum
+
+from app.schemas.usuario import UsuarioOut
 
 class EstadoDeseo(str, Enum):
     pendiente = "pendiente"
@@ -12,14 +15,16 @@ class DeseoBase(BaseModel):
     link: Optional[str] = None
     descripcion: Optional[str] = None
     estado: Optional[EstadoDeseo] = EstadoDeseo.pendiente
+    fecha_creacion: Optional[date ] = None
 
 class DeseoCreate(DeseoBase):
     pass
-
+    
 class DeseoOut(DeseoBase):
     id_deseo: int
     id_usuario: int
     id_familia: int
-
+    usuario: UsuarioOut
+    
     class Config:
         from_attributes = True
