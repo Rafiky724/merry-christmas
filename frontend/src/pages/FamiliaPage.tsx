5d492ef272c405433d1c5715c0b31335d8faff76
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-09-01 20:00:26 -0500

    Update FamiliaPage.tsx

--- frontend/src/pages/FamiliaPage.tsx
+++ frontend/src/pages/FamiliaPage.tsx
@@ -1,8 +1,20 @@
 import DeseoCard from "../components/DeseoCard";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import { useQuery } from "@tanstack/react-query";
+import { getDeseosFamilia } from "../services/deseoService";
+import type { Deseo } from "../types";
 
 export default function FamiliaPage() {
+  const { data: deseos, isLoading } = useQuery<Deseo[]>({
+    queryKey: ["deseosFamilia"],
+    queryFn: getDeseosFamilia,
+  });
+
+  if (isLoading) {
+    return <p className="text-center">Cargando deseos...</p>;
+  }
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
@@ -10,7 +22,9 @@ export default function FamiliaPage() {
 
         <main className="flex-grow p-5 bg-[url('/patron_navidad.png')] bg-[length:250px_250px] bg-repeat">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-            <DeseoCard />
+            {deseos?.map((deseo) => (
+              <DeseoCard key={deseo.id_deseo} deseo={deseo} />
+            ))}
           </div>
         </main>
 
