c3e0b98f2f92396105b1024a6ec6ec364cc46a1a
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-10-18 22:49:53 -0500

    Update DeseoCard.tsx

--- frontend/src/components/DeseoCard.tsx
+++ frontend/src/components/DeseoCard.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import type { Deseo } from "../types";
 import { useState } from "react";
 import { cambiarEstadoDeseo, eliminarDeseo } from "../services/deseoService";
